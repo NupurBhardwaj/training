97274fd46737e192fe506953752dba8a5a224725
NupurBhardwaj <76148660+NupurBhardwaj@users.noreply.github.com>
2020-12-17 13:41:18 +0530

    first commit

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+class Calculator{
+       public static int add(int num1,num2){
+         return num1+num2;
+       }
+       
+       public static void main(String[] args){
+        System.out.println(add(10,30));
+       }
+       
+}
