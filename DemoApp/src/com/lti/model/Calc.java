1f2ffe3f73850dff9fdea8719a0d74a1c126b019
NupurBhardwaj <76148660+NupurBhardwaj@users.noreply.github.com>
2020-12-18 13:05:43 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -6,6 +6,6 @@ public class Calc {
      }
      
      public void subs(int num1,int num2) {
-    	 System.out.println("Difference= "+(num1-num2));
+    	 System.out.println("Difference is= "+(num1-num2));
      }
 }
