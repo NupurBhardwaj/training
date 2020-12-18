25344ae6348589950b34b1a62aa42f24f3cba5b7
NupurBhardwaj <nupurbhardwaj3008@gmail.com>
2020-12-18 15:49:01 +0530

    commit new

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -4,5 +4,7 @@ public class Calc {
      public void add(int num1,int num2) {
     	 System.out.println("Sum= "+(num1+num2));
      }
-    
+     public void subs(int num1,int num2) {
+    	 System.out.println("Diff= "+(num1-num2));
+     }
 }
